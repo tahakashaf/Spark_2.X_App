11ca2dc9a0909ba318b5418a5b2fe518aeb69178
kashaf <tahakashaf@gmail.com>
2018-08-17 02:33:58 +0530

    added spark sbt dependencies

--- build.sbt
+++ build.sbt
@@ -4,4 +4,9 @@ version := "1.0"
 
 scalaVersion := "2.11.12"
 
-libraryDependencies += "org.apache.spark" %% "spark-core" % "2.2.0"
+val SPARK_VERSION = "2.2.0"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % SPARK_VERSION
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % SPARK_VERSION
+libraryDependencies += "org.apache.spark" %% "spark-sql" % SPARK_VERSION
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % SPARK_VERSION
---
+++ src/main/scala/SparkScala.scala
@@ -0,0 +1,19 @@
+/**
+  * Created by NAQVI on 8/17/2018.
+  */
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.log4j.{Level, Logger}
+object SparkScala {
+
+  Logger.getLogger("org").setLevel(Level.ERROR)
+  //configuration variable can be defined
+  val sparkConf = new SparkConf()
+  sparkConf.setAppName("Taha's Spark Application")
+  //local mode :- 4 specifies the logical cores of the CPU
+  sparkConf.setMaster("local[4]")
+  val sparkContext = new SparkContext(sparkConf)
+  println(sparkContext)
+  System.in.read
+
+
+}
